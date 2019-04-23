793a510a9d1bec3ccb96332af1a0e43e3d31b81b
Haozhe Gu  (ASH) <ashghz26@gmail.com>
2019-04-22 19:53:14 -0700

    Solve Thread problem in MediaPlayer, and initial structure for AdRemoval

--- Java/AdFilter/AdRemoval/src/AdRemoval.java
+++ Java/AdFilter/AdRemoval/src/AdRemoval.java
@@ -1,9 +1,21 @@
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 
 public class AdRemoval {
+	String dataset;
+	String videoPath;
+	String audioPath;
+	
 	private double getShannonEntropy(BufferedImage actualImage){
 		 List<String> values= new ArrayList<String>();
 		             int n = 0;
@@ -34,5 +46,53 @@ public class AdRemoval {
 		             e += p * (Math.log(p)/Math.log(2));
 		        }
 		 return -e;
+	}
+	
+	private void resolveArguments(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Invalid Number of Input Arguments");
+			System.exit(-1);
+		} else {
+			dataset = args[0];
+			//AdRemoval.java -> AdFilter folder -> Java folder -> Project folder -> Data folder..
+			String dataDir = "../../../Data/" + dataset + "/Videos/";
+			List<String> fileList = new ArrayList<String>();
+
+			try (Stream<Path> paths = Files.walk(Paths.get(dataDir))) {
+				fileList = paths.filter(path -> path.toString().endsWith(".rgb") || path.toString().endsWith(".wav"))
+						.map(path -> path.toAbsolutePath().normalize().toString()).collect(Collectors.toList());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			if (fileList.size() == 2) {
+				videoPath = fileList.get(0).endsWith(".rgb") ? fileList.get(0) : fileList.get(1);
+				audioPath = fileList.get(0).endsWith(".wav") ? fileList.get(0) : fileList.get(1);
+				
+				
+			} else {
+				System.out.println("Invalid Number of Valid Files in Given Dataset");
+				System.exit(-1);
+			}
+
+		}
+	}
+	
+	private void displayConfiguration() {
+		String seperator = "--------------------";
+		System.out.println(seperator + "Parsing Input Argument" + seperator);
+		System.out.println("Parsing Input Argument -> OK");
+		System.out.println("Dataset: " + dataset);
+		System.out.println("Video file: " + videoPath);
+		System.out.println("Audio file: " + audioPath);
+		System.out.println("Output Directory Path: "+outputPath);
+	}
+	
+	public static void main(String[] args) {
+		AdRemoval remover = new AdRemoval();
+		
+		/* Resolve Input Arguments */
+		remover.resolveArguments(args);
+		remover.displayConfiguration();
 	}
 }
--- Java/MediaPlayer/src/run.sh
+++ Java/MediaPlayer/src/run.sh
@@ -2,11 +2,11 @@ echo -e "-------------------- Compiling For Player --------------------"
 
 echo -e "---------- Compiling For VideoPlayer ----------"
 
-javac videoplayer/VideoPlayer.java
+javac -d . videoplayer/VideoPlayer.java
 
 echo -e "---------- Compiling For AudioPlayer ----------"
 
-javac audioplayer/AudioPlayer.java
+javac -d . audioplayer/AudioPlayer.java
 
 echo -e "---------- Compiling For MediaPlayer ----------"
 
